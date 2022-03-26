3f06104d0210a2efed989743c29d893701db940f
fauzan07 <fauzanfalke@gmail.com>
2022-03-26 15:15:07 +0530

    form updated

--- script.js
+++ script.js
@@ -1,45 +1,7 @@
 $(document).ready(function(){
-    $("#submit_form").validate({
-       rules: {
-                customer_name: {
-                    required: true,
-                    minlength: 3,
-                    maxlength: 35,
-                },
-                phone: {
-                    required: true,
-                    minlength: 10,
-                    maxlength: 10,
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                rate_quality:"required"
-            },
-
-            messages: {
-                customer_name: {
-                    required: "Customer Name is required",
-                    letters: "Must enter character only",
-                    maxlength: "Please enter 35 Characters only"
-                },
-                email: {
-                    required: "Email is required",
-                    email: "Please enter a valid email id",
-                    emailtest: "Please enter a valid email address"
-                },
-                phone: {
-                    required: "Please enter a valid mobile number",
-                    minlength: "Please enter 10 digits only",
-                    maxlength: "Please enter 10 digits only"
-                },
-            }
-    });
-});
-
-$(document).ready(function(){
     $(document).on('click','#sava_data',function(){
+    var err = "";
+    var alert = "";
     var person = {
         cus_name: "",
         email: "",
@@ -51,6 +13,7 @@ $(document).ready(function(){
     };
 
     // function savedata(){
+
     var lscount = localStorage.length;
         person.cus_name = $("#customer_name").val(),
         person.email = $("#email").val(),
@@ -60,9 +23,24 @@ $(document).ready(function(){
         person.restaurant_clean = $("#restaurant_clean:checked").val(),
         person.overall_dining = $("#overall_dining:checked").val(),
 
-        localStorage.setItem("rleads" + lscount,  JSON.stringify(person));
+        person.cus_name.length ? true : err += " Please provide valid name. " ;
+        person.email.length ? true : err += " Please provide valid email. " ;
+        person.phone.length ? true : err += " Please provide valid Phone Number. " ;
+        person.rate_quality = $("#rate_quality:checked").length ? person.rate_quality = $("#rate_quality:checked").val() : err += " Please Select rate quality. " ;
+        person.beverage = $("#beverage:checked").length ?  person.beverage = $("#beverage:checked").val() : err += " Please select beverage. " ;
+        person.restaurant_clean = $("#restaurant_clean:checked").length ? person.restaurant_clean = $("#restaurant_clean:checked").val() : err += " Please select is resturent clean. " ;
+        person.overall_dining = $("#overall_dining:checked").length ?    person.overall_dining = $("#overall_dining:checked").val() : err += " Please select overall experience. " ;
 
-        window.location = "thankyou.html";
+        if(!err.length){
+            localStorage.setItem("rleads" + lscount,  JSON.stringify(person));
+            window.location = "thankyou.html";
+        }else{
+            // alert("<p class='text-danger'>"+err+"</p>");
+            alert += "<div class='alert alert-danger' role='alert'>"+err+"</div>";
+            window.scrollTo(xCoord, yCoord);
+    }
+    var alertdata = $('#alert-err').find("p");
+    alertdata.append(alert);
 // }
     });
 
@@ -79,7 +57,7 @@ $(document).ready(function(){
             var rleads = localStorage.getItem(key);
             var data = JSON.parse(rleads);
             console.log(data.cus_name);
-            trs += '<tr><td>'+data.cus_name+'</td><td>'+data.email+'</td><td>'+data.phone+'</td><td>'+data.rate_quality+'</td></tr>';
+            trs += '<tr><td>'+data.cus_name+'</td><td>'+data.email+'</td><td>'+data.phone+'</td><td>'+data.rate_quality+'</td><td>'+data.beverage+'</td><td>'+data.restaurant_clean+'</td><td>'+data.overall_dining+'</td></tr>';
             }
         // });
      }
